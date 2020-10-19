cdfe11f455b91ec6b1716896b4e96c2c258920e8
zaidanali028 <zaidanali028@gmail.com>
2020-10-19 22:18:35 +0100

    currently on the delete all route

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-//const randomPort = Math.floor(Math.random() * 3000);
-
+const methodOverride=require('method-override')
 const port = 2000 || process.env.PORT;
 const ejs = require("ejs");
 const mongoose = require("mongoose");
@@ -21,16 +20,20 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
  //static files
 app.use(express.static(path.join(__dirname, "./public")));
+//method override to be used for sending put requests
+app.use(methodOverride('_method'))
 
 //view engine
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("view engine", "ejs",);
 app.set(express.static(path.join(__dirname, "public")));
 
 
 const indexRoute = require("./routes/home/index");
 const adminRoute = require("./routes/admin/admin");
 const postsRoute = require("./routes/posts/posts");
+
+//requiring helper function
 
 app.use("/", indexRoute);
 app.use("/admin", adminRoute);
--- routes/posts/posts.js
+++ routes/posts/posts.js
@@ -1,39 +1,120 @@
-const express=require('express')
-const router=express.Router()
-
+const express = require("express");
+const Postschema = require("../../models/Posts");
+const router = express.Router();
+const faker = require("faker");
 //Schema
-const postSchema=require('../../models/Posts')
-router.get('/',(req,res)=>{
+const postSchema = require("../../models/Posts");
 
-  res.render('admin/allPosts')
-    //  res.render('admin/allPosts')
-  })
-  
-  router.get('/create',(req,res)=>{
-    res.render('admin/createPosts')
-    // res.send('create posts route on fire')
+router.get("/", (req, res) => {
+  postSchema.find({}).then((posts) => {
+    res.render("admin/index", {
+      allPosts: posts,
+    });
+  });
+});
 
-  })
+router.get("/create", (req, res) => {
+  res.render("admin/createPosts");
+  // res.send('create posts route on fire')
+});
 
-  router.post('/create',(req,res)=>{
-    let {title,status,allowComments,description}=req.body
-    //handling allowcomments
-    if(allowComments){
-      allowComments=true
-    }else{
-      allowComments=false
+router.post("/create", (req, res) => {
+  const { title, status, description } = req.body;
+
+  //handling allowcomments(if is on,we will overwrite on with true and false otherwise)
+  if (allowComments) {
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
+  const newPost = new postSchema({
+    title,
+    status,
+    allowComments,
+    description,
+  });
+  newPost.save().then((newPostSaved) => {
+    res.redirect("/admin/posts");
+  });
+});
+
+router.get("/edit/:id", (req, res) => {
+  const { id } = req.params;
+  postSchema.findOne({ _id: id }).then((userFound) => {
+    res.render("admin/editPost", {
+      gotUser: userFound,
+    });
+  });
+});
+
+router.put("/edit/:id", (req, res) => {
+  const { id } = req.params;
+  let { title, status, description } = req.body;
+  if (allowComments) {
+    allowComments = true;
+  } else {
+    allowComments = false;
+  }
+
+  Postschema.findByIdAndUpdate(
+    { _id: id },
+    {
+      $set: {
+        title: title,
+        status: status,
+        allowcomments: allowComments,
+        description: description,
+      },
     }
-    const newPost=new postSchema({
-        title,
-        status,
-        allowComments,
-        description
-    })
-    newPost.save()
-    .then(newPostSaved=>{
-        res.redirect('/admin/posts')
-    })
-    // res.send('Create Posts')
-  })
+  ).then((updatedPost) => {
+    res.redirect("/admin/posts");
+  });
+});
 
- module.exports=router
+router.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  postSchema.findByIdAndDelete({ _id: id }).then((thePosts) => {
+    thePosts.remove();
+    res.redirect("/admin/posts");
+  });
+});
+
+//dummpy data
+router.get("/dummy", (req, res) => {
+  res.render("admin/allPosts");
+});
+
+router.post("/dummy", (req, res) => {
+        for (let i = 0; i < req.body.amount; i++) {
+    const fakePost = new postSchema({
+      title: faker.name.title(),
+      status: "private",
+      allowComments: faker.random.boolean(),
+      description: faker.lorem.sentences(),
+    });
+    fakePost
+      .save()
+      .then((faked) => {
+        res.redirect("/admin/posts");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
+
+router.delete("/clear", (req, res) => {
+  //find{} finds all posts
+  postSchema.find({}).then((allposts) => {
+    //allposts are all posts in the database
+    for (post of allposts) {
+      //then here,for anypoost of all the posts,I got its id
+      const postId = post.id;
+      postSchema.findByIdAndDelete({ _id: postId }).then((gottenPost) => {
+        gottenPost.remove();
+      });
+    }
+    res.redirect("/admin/posts");
+  });
+});
+module.exports = router;
